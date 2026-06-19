288dec29867fa1b16d4c8bb31a576399d551c08f
josemancebo0507 <sperancin.ads@gmail.com>
2026-06-18 21:40:00 -0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -5,8 +5,8 @@ export async function middleware(request: NextRequest) {
   let supabaseResponse = NextResponse.next({ request })
 
   const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    'https://ehbosnplrsixaloivtbe.supabase.co',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVoYm9zbnBscnNpeGFsb2l2dGJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE4MTEwNjAsImV4cCI6MjA5NzM4NzA2MH0.q-m2sx91Ut5HdjTc8mPoG6a13f5C1kNsKInzD__3YT8',
     {
       cookies: {
         getAll() { return request.cookies.getAll() },
@@ -23,12 +23,10 @@ export async function middleware(request: NextRequest) {
 
   const { data: { user } } = await supabase.auth.getUser()
 
-  // Rota /admin exige autenticação
   if (request.nextUrl.pathname.startsWith('/admin') && !user) {
     return NextResponse.redirect(new URL('/login', request.url))
   }
 
-  // Já autenticado tentando acessar /login → redireciona para admin
   if (request.nextUrl.pathname === '/login' && user) {
     return NextResponse.redirect(new URL('/admin', request.url))
   }
